09975a49e6f9620180489734090d9ebd7ec04e3e
Shannen <rshannenjosh@gmail.com>
2025-11-22 09:49:06 +0530

    update all files

--- api/_utils.py
+++
@@ -1,9 +0,0 @@
-# Simple helper for JSON responses
-import json
-
-def response(body, status=200):
-    return {
-        "statusCode": status,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps(body)
-    }
